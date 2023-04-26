f9fc58b4ad0941546322c14d5dd3a79420f3eb01
Rick <54125370+CindyAndRick@users.noreply.github.com>
2023-04-26 19:40:17 +0800

    update backend: init database

--- spider/getHisPM25Data.py
+++ spider/getHisPM25Data.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def nextdate(y, m, d):
     big = [1, 3, 5, 7, 8, 10, 12]
     small = [4, 6, 9, 11]
@@ -15,20 +16,23 @@ def nextdate(y, m, d):
         d = d + 1
     return y, m, d
 
+
 if __name__ == '__main__':
     y = 2020
     m = 1
     d = 1
     baseUrl = 'https://quotsoft.net/air/data/china_cities_'
-    headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0'}
-    while(1):
-        url = baseUrl + str(y) + (('0' + str(m)) if m < 10 else str(m)) + (('0' + str(d)) if d < 10 else str(d)) + '.csv'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0'}
+    while (1):
+        url = baseUrl + str(y) + (('0' + str(m)) if m < 10 else str(m)) + \
+            (('0' + str(d)) if d < 10 else str(d)) + '.csv'
         r = requests.get(url, headers=headers)
-        '''
-        with open(file='./spider/data{}.csv'.format(str(y)+str(m)+str(d)), mode='w+', encoding='utf-8') as f:
+
+        with open(file='./data{}.csv'.format(str(y)+str(m)+str(d)), mode='w+', encoding='utf-8') as f:
             f.write(r.text)
             f.close()
-        '''
+        break
         y, m, d = nextdate(y, m, d)
         # print(y,m,d)
         # if y == 2020 and m == 4 and d == 18:
